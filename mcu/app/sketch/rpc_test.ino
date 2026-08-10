a8fb760e63cefb25daea58162ebcdd2283794484
NSIL IAEA - Ivan Morales <nsil@iaea.org>
2026-08-10 09:22:26 +0000

    Issue #99: add Arduino App/Sketch skeleton

---
+++ mcu/app/sketch/rpc_test.ino
@@ -0,0 +1,31 @@
+#include "Arduino_RouterBridge.h"
+
+const int TEMP_PIN = A0;
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    
+    Bridge.begin();
+    Bridge.provide("read_temperature", read_temperature);
+    Bridge.provide("set_led_state", set_led_state);
+    
+    Monitor.begin();
+    Monitor.println("Temperature sensor ready");
+}
+
+void loop() {}
+
+float read_temperature() {
+    int raw = analogRead(TEMP_PIN);
+    float voltage = (raw / 16383.0) * 3.3;
+    float temp_c = (voltage - 0.5) * 100.0;
+    
+    Monitor.print("Temperature: ");
+    Monitor.println(temp_c);
+    
+    return temp_c;
+}
+
+void set_led_state(bool state) {
+    digitalWrite(LED_BUILTIN, state ? LOW : HIGH);
+}
