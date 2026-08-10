1fcab592621461e4030b0f92b2ac0cf2bff6468e
Ivan Morales <ivan.rene.morales@gmail.com>
2026-08-10 17:52:24 +0200

    Issue #101: update Arduino sketch, not tested yet

--- mcu/app/rpc_test/rpc_test.ino
+++ mcu/app/rpc_test/rpc_test.ino
@@ -1,34 +1,82 @@
 #include "Arduino_RouterBridge.h"
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
 
+ArduinoLEDMatrix matrix;
+
+const uint16_t BUF_SIZE = 256;
+char messageBuffer[BUF_SIZE] = "Starting RIID...";
 
 const int TEMP_PIN = A0;
-const int LED_PIN = LED4_G;
+const int LED_R = LED4_R;
+const int LED_G = LED4_G;
+const int LED_B = LED4_B;
 
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_R, OUTPUT);
+    pinMode(LED_G, OUTPUT);
+    pinMode(LED_B, OUTPUT);
+
+    digitalWrite(LED_R, 1);
+    digitalWrite(LED_G, 1);
+    digitalWrite(LED_B, 1);
+
+    matrix.begin();
     
     Bridge.begin();
-    Bridge.provide("read_temperature", read_temperature);
-    Bridge.provide("set_led_state", set_led_state);
+    Bridge.provide("update_text_matrix", update_text_matrix);
+    Bridge.provide("update_status_led", update_status_led);
     
     Monitor.begin();
     Monitor.println("Temperature sensor ready");
+
+    
 }
 
-void loop() {}
+void loop() {
+    Bridge.update();
+    matrix.beginDraw();
+    matrix.stroke(0xFFFFFFFF);
+    matrix.textFont(Font_5x7);
+    matrix.textScrollSpeed(40);
 
-float read_temperature() {
-    int raw = analogRead(TEMP_PIN);
-    float voltage = (raw / 16383.0) * 3.3;
-    float temp_c = (voltage - 0.5) * 100.0;
-    
-    Monitor.print("Temperature: ");
-    Monitor.println(temp_c);
-    
-    return temp_c;
+    matrix.beginText(0, 1, 0xFFFFFF);
+    matrix.print(messageBuffer);
+    matrix.endText(SCROLL_LEFT);
+    matrix.endDraw();
 }
 
-void set_led_state(bool state) {
-    digitalWrite(LED_PIN, state ? LOW : HIGH);
+void update_status_led(int status) {
+    switch(status) {
+        case 0:
+            digitalWrite(LED_R, 1);
+            digitalWrite(LED_G, 1);
+            digitalWrite(LED_B, 0);
+            break;
+        case 1:
+            digitalWrite(LED_R, 0);
+            digitalWrite(LED_G, 1);
+            digitalWrite(LED_B, 1);
+            break;
+        case 2:
+            digitalWrite(LED_R, 1);
+            digitalWrite(LED_G, 0);
+            digitalWrite(LED_B, 1);
+            break;
+        case 3:
+            digitalWrite(LED_R, 1);
+            digitalWrite(LED_G, 0);
+            digitalWrite(LED_B, 0);
+            break;
+        default:
+            digitalWrite(LED_R, 1);
+            digitalWrite(LED_G, 1);
+            digitalWrite(LED_B, 1);
+    }
+}
+
+void update_text_matrix(String text) {
+    strncpy(messageBuffer, text.c_str(), BUF_SIZE - 5);
+    strcat(messageBuffer, "    ");
 }
